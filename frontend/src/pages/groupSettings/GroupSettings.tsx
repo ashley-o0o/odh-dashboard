3d1d41a518e0748ec2a3e8e17210585ae980f1c4
Ashley McEntee <amcent@redhat.com>
2024-07-02 15:35:15 -0400

    Update depricated PF multiselect

--- frontend/src/pages/groupSettings/GroupSettings.tsx
+++ frontend/src/pages/groupSettings/GroupSettings.tsx
@@ -11,9 +11,9 @@ import {
 import ApplicationsPage from '~/pages/ApplicationsPage';
 import { isGroupEmpty } from '~/utilities/utils';
 import SettingSection from '~/components/SettingSection';
-import { MultiSelection } from '~/components/MultiSelection';
+import { MultiSelection, SelectionOptions } from '~/components/MultiSelection';
 import { useWatchGroups } from '~/utilities/useWatchGroups';
-import { GroupsConfigField, MenuItemStatus } from './groupTypes';
+import { GroupsConfigField } from './groupTypes';
 
 const GroupSettings: React.FC = () => {
   const {
@@ -37,13 +37,27 @@ const GroupSettings: React.FC = () => {
     updateGroups(groupSettings);
   };
 
-  const handleMenuItemSelection = (newState: MenuItemStatus[], field: GroupsConfigField) => {
+  const handleMenuItemSelection = (newState: SelectionOptions[], field: GroupsConfigField) => {
     switch (field) {
       case GroupsConfigField.ADMIN:
-        setGroupSettings({ ...groupSettings, adminGroups: newState });
+        setGroupSettings({
+          ...groupSettings,
+          adminGroups: newState.map((opt) => ({
+            id: Number(opt.id),
+            name: opt.name,
+            enabled: opt.selected || false,
+          })),
+        });
         break;
       case GroupsConfigField.USER:
-        setGroupSettings({ ...groupSettings, allowedGroups: newState });
+        setGroupSettings({
+          ...groupSettings,
+          allowedGroups: newState.map((opt) => ({
+            id: Number(opt.id),
+            name: opt.name,
+            enabled: opt.selected || false,
+          })),
+        });
         break;
     }
     setIsGroupSettingsChanged(true);
